fe1abbbb427662aad9e3eb910de422007022da1b
n.vaneeva <n.vaneeva@keep-calm.ru>
2023-02-01 17:29:32 +0300

    FIX: fixed name of arg of calc method

--- Main.java
+++ Main.java
@@ -6,15 +6,15 @@ class Main {
         System.out.println(calc(args[0]));
     }
 
-    public static String calc(String expression) {
+    public static String calc(String input) {
         String errorMessage = "Выражение может состоять только из 2-x чисел от 1 до 10 включительно и символов: +, -, *, /";
-        if (expression == null) {
+        if (input == null) {
             throw new RuntimeException("Вероятно вы передали в качестве выражения null. " + errorMessage);
         }
 
-        if (new Validator().validate(expression)) {
-            String[] arr = expression.split("\\s?(\\+|-|\\*|\\/)\\s?");
-            String symbol = expression.replaceAll("[0-9]|\\s?", "");
+        if (new Validator().validate(input)) {
+            String[] arr = input.split("\\s?(\\+|-|\\*|\\/)\\s?");
+            String symbol = input.replaceAll("[0-9]|\\s?", "");
             int num0 = Integer.parseInt(arr[0]);
             int num1 = Integer.parseInt(arr[1]);
             return new Calculator().calculator(num0, num1, symbol);
